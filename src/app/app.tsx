10b9cf14a22fffe51510ff4fe975941e50a229a0
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-05-30 18:42:45 +0400

    update base project structure; delete extra files

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+import './app.css';
+
+export const  App = () => (
+    <>
+      <header className="app__header">
+        Tools
+      </header>
+      <main className="app__content">
+        Canvas
+      </main>
+    </>
+   
+  );
+
+
