bb2dcd4c08947cddf4d2df9f2f148fd76ad79a2b
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-05-30 18:42:58 +0400

    create shared resizable-container component

---
+++ src/shared/components/resizable-container/resizable-container.tsx
@@ -0,0 +1,36 @@
+import { ReactNode, useEffect, useRef, useState } from 'react';
+
+interface ContainerDimensions {
+  width: number;
+  height: number;
+}
+
+interface ResizableContainerProps {
+  children: (dimensions: ContainerDimensions) => ReactNode;
+}
+
+export const ResizableContainer = ({ children }: ResizableContainerProps) => {
+  const [dimensions, setDimensions] = useState<ContainerDimensions | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current: container } = containerRef;
+
+    if (container) {
+      const calculateDimensions = () => {
+        const { offsetWidth, offsetHeight } = container;
+
+        setDimensions({ width: offsetWidth, height: offsetHeight });
+      }
+
+      calculateDimensions(); // Dimensions initialization
+
+      window.addEventListener("resize", calculateDimensions);
+      return () => window.removeEventListener("resize", calculateDimensions);
+    }
+  }, [])
+
+  return <div ref={containerRef} style={{ width: "100%", height: "100%" }}>
+    {dimensions && children(dimensions)}
+  </div>
+}
