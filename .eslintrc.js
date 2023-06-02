6691c4e0ea1193776586c343b40bf8b045c93c43
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-06-02 17:36:12 +0400

    configure prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["./tsconfig.json", "./tsconfig.eslint.json"] },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "auto", // default: "lf"
+  jsxSingleQuote: false,
+  printWidth: 120, // default: 80
+  quoteProps: "as-needed",
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
