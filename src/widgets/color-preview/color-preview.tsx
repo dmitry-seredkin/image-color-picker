f9d7105a28a2b02b794149ad85bf34297ffde28b
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-06-02 21:45:19 +0400

    update color-preview widget; expand global css-variables

--- src/widgets/color-preview/color-preview.tsx
+++ src/widgets/color-preview/color-preview.tsx
@@ -1,22 +1,18 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes } from "react";
 
-import { Button } from '../../shared/components';
-import { classnames as cn, writeToClipboard } from '../../shared/utils';
+import { ContentCopyIcon } from "../../shared/components/icons";
+import { classnames as cn } from "../../shared/utils";
 
 import s from "./color-preview.module.css";
 
-type Color = string;
-
 interface ColorPreviewProps extends HTMLAttributes<HTMLDivElement> {
-  color: Color;
+  color: string;
 }
 
 export const ColorPreview = ({ className, color, ...props }: ColorPreviewProps) => (
-  <div className={cn(className, s.container)} {...props}> 
+  <div className={cn(className, s.container)} {...props}>
     <div className={s.preview} style={{ backgroundColor: color }} />
-    <div className={s.wrapper}>
-      <p className={s.color}>{color}</p>
-      <Button onClick={() => writeToClipboard(color)}>Copy</Button>
-    </div>
+    <p className={s.color}>{color}</p>
+    <ContentCopyIcon />
   </div>
 );
