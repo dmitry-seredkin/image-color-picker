8cdd89462278dab97521b30c3c8a0361e3b3b71e
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-06-02 11:50:30 +0400

    create color-dropper widget

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,36 @@
-import './app.css';
+import { useState } from 'react';
 
-export const  App = () => (
+import { Button, ResizableContainer } from '../shared/components';
+import { ColorPreview } from '../widgets/color-preview';
+import { ColorDropper } from '../widgets/color-dropper';
+
+import s from "./app.module.css";
+import './global.css';
+
+export const  App = () => {
+  const [isDropperActive, setIsDropperActive] = useState<boolean>(false)
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+
+  const toggleDropper = () => setIsDropperActive(prev => !prev);
+  
+  return (
     <>
-      <header className="app__header">
-        Tools
+      <header className={s.header}>
+        <Button onClick={toggleDropper}>Dropper</Button>
+        {selectedColor && <ColorPreview color={selectedColor} />}
       </header>
-      <main className="app__content">
-        Canvas
+      <main className={s.content}>
+        <ResizableContainer>
+          {(sizes) => <div>{sizes.width} {sizes.height}</div>}
+        </ResizableContainer>
       </main>
+      {isDropperActive && <ColorDropper colors={[
+        ["#ff0", "#f0f", "#ff0"],
+        ["#ff0", "#f0f", "#ff0"],
+        ["#ff0", "#f0f", "#ff0"],
+    
+    ]} position={{ x: 300, y: 300 }} />}
     </>
    
   );
-
-
+}
