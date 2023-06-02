0b4b92a3155460d4f224f7923f380ba9967901ca
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-06-02 21:02:57 +0400

    create shared icon component; add color-picker and content-copy icons

---
+++ src/shared/components/icons/icon/icon.tsx
@@ -0,0 +1,11 @@
+import { FC, SVGProps } from "react";
+import { classnames as cn } from "../../../utils";
+
+import s from "./icon.module.css";
+
+type IconProps = SVGProps<SVGSVGElement>;
+
+export const Icon =
+  (IconComponent: FC<IconProps>) =>
+  ({ className, ...props }: IconProps) =>
+    <IconComponent className={cn(className, s.icon)} {...props} />;
---
+++ src/shared/components/icons/index.ts
@@ -0,0 +1,6 @@
+import { Icon } from "./icon";
+import { ReactComponent as ColorPicker } from "./svg/color-picker.svg";
+import { ReactComponent as ContentCopy } from "./svg/content-copy.svg";
+
+export const ColorPickerIcon = Icon(ColorPicker);
+export const ContentCopyIcon = Icon(ContentCopy);
